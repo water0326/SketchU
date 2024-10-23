8e678f733e8833bd29dcb393123d029ea4d980d7
water0326 <bounce0326@naver.com>
2024-10-23 19:26:18 +0900

    feat: add structure

---
+++ sketch_u/src/app/_components/navigation.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+    min-width: 300px;
+    max-width: 300px;
+    width: 300px;
+    height: 100vh;
+    background-color: #90D8BF;
+    box-shadow: 3px 0px 4px rgba(0,0,0,0.15);
+`;
+
+
+export default function Navigation() {
+    return (
+      <Container></Container>
+    );
+}
