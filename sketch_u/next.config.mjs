4484c98116cb92d9c2de8b590e9dbb66a7d1d353
water0326 <bounce0326@naver.com>
2024-12-04 14:29:51 +0900

    fix: fix url 5

--- sketch_u/next.config.mjs
+++ sketch_u/next.config.mjs
@@ -3,12 +3,15 @@ const nextConfig = {
     async rewrites() {
         const destination = 'http://43.201.55.125:8081/api/:path*';
         
-        console.log('API Destination:', destination);
-        
         return [
           {
             source: '/api/:path*',
             destination: destination,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            },
           },
         ];
     }
