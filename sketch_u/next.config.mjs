30cdd3f82863ac4005843e21f391697b7110478d
water0326 <bounce0326@naver.com>
2024-12-04 14:24:30 +0900

    fix: fix url 4

--- sketch_u/next.config.mjs
+++ sketch_u/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
+        const destination = 'http://43.201.55.125:8081/api/:path*';
+        
+        console.log('API Destination:', destination);
+        
         return [
           {
             source: '/api/:path*',
-            destination: process.env.NEXT_PUBLIC_API_URL 
-              ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-              : 'http://43.201.55.125:8081/api/:path*',
+            destination: destination,
           },
         ];
     }
